c0c079070de866191714e9d4e5213430c4418de2
Mathias Buus <mathiasbuus@gmail.com>
2013-07-28 21:36:07 +0200

    safety check that a nodes belongs to the current list

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var Node = function(val) {
+var Node = function(list, val) {
 	this.prev = this.next = this;
 	this.value = val;
+	this.list = list;
 };
 
 Node.prototype.link = function(next) {
@@ -16,20 +17,23 @@ var FIFO = function() {
 };
 
 FIFO.prototype.set = function(node, value) {
+	if (!node || node.list !== this) return null;
 	node.value = value;
 	return node;
 };
 
 FIFO.prototype.get = function(node) {
+	if (!node || node.list !== this) return null;
 	return node.value;
 };
 
 FIFO.prototype.remove = function(node) {
-	if (!node) return;
-	if (node !== node.next || this.node === node) this.length--;
+	if (!node || node.list !== this) return null;
+	this.length--;
+	node.list = null;
 	node.prev.link(node.next);
 	if (node === this.node) this.node = node.next === node ? null : node.next;
-	return node.link(node).value;
+	return node.value;
 };
 
 FIFO.prototype.unshift = function(value) {
@@ -37,7 +41,7 @@ FIFO.prototype.unshift = function(value) {
 };
 
 FIFO.prototype.push = function(value) {
-	var node = new Node(value);
+	var node = new Node(this, value);
 	this.length++;
 	if (!this.node) return this.node = node;
 	this.node.prev.link(node);
